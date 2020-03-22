8dc684d78d9d373756fb19f82c32586841e83481
Paweł Broda <pwl.broda@gmail.com>
2020-03-22 21:41:36 +0100

    [draft] java with vavr

---
+++ 2020-03-21-java-with-vavr-case-study/src/test/java/com/pbroda/codesnippets/vavr/LoopBasics.java
@@ -0,0 +1,134 @@
+package com.pbroda.codesnippets.vavr;
+
+import io.vavr.collection.List;
+import io.vavr.control.Either;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.vavr.collection.Stream;
+
+import lombok.Value;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static io.vavr.API.Some;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@Value
+class ProcessingResult {
+    private final int id;
+    private final boolean result;
+}
+
+class AnyService {
+    public ProcessingResult serviceCall(ProcessingResult pr) {
+        return pr;
+    }
+}
+
+interface AnyInterface {
+    Either<Throwable, ProcessingResult> serviceCall();
+}
+
+@ExtendWith(MockitoExtension.class)
+public class LoopBasics {
+
+    Stream<ProcessingResult> testStream;
+    List<ProcessingResult> testList;
+
+    ProcessingResult pr1 = new ProcessingResult(1, true);
+    ProcessingResult pr2 = new ProcessingResult(2, true);
+    ProcessingResult pr3 = new ProcessingResult(3, false);
+    ProcessingResult pr4 = new ProcessingResult(4, false);
+    ProcessingResult pr5 = new ProcessingResult(5, true);
+    ProcessingResult pr6 = new ProcessingResult(6, true);
+
+    AnyService anyServiceStream;
+    AnyService anyServiceList;
+
+    @BeforeEach
+    void init() {
+        testStream = Stream.of(pr1, pr2, pr3, pr4, pr5, pr6);
+        testList = List.of(pr1, pr2, pr3, pr4, pr5, pr6);
+
+        anyServiceStream = spy(AnyService.class);
+        anyServiceList = spy(AnyService.class);
+    }
+
+    @Test
+    void testTakeWhile() {
+        var resultStream = testStream.map(
+                pr -> anyServiceStream.serviceCall(pr)
+            )
+            .takeWhile(processingResult -> processingResult.isResult());
+
+        var resultList = testList.map(
+                pr -> anyServiceList.serviceCall(pr)
+            )
+            .takeWhile(processingResult -> processingResult.isResult());
+
+        verify(anyServiceStream, times(1)).serviceCall(any()); // result of implementation Stream as lazy linked list
+        verify(anyServiceList, times(6)).serviceCall(any());
+
+        assertEquals(List.of(pr1, pr2), resultStream);
+        assertEquals(List.of(pr1, pr2), resultList);
+    }
+
+    @Test
+    void testDropWhile() {
+        var resultStream = testStream.map(
+                pr -> anyServiceStream.serviceCall(pr)
+            )
+            .dropWhile(processingResult -> processingResult.isResult());
+
+        var resultList = testList.map(
+                pr -> anyServiceList.serviceCall(pr)
+            )
+            .dropWhile(processingResult -> processingResult.isResult());
+
+        verify(anyServiceStream, times(3)).serviceCall(any()); // result of implementation Stream as lazy linked list
+        verify(anyServiceList, times(6)).serviceCall(any());
+
+        assertEquals(List.of(pr3, pr4, pr5, pr6), resultStream);
+        assertEquals(List.of(pr3, pr4, pr5, pr6), resultList);
+    }
+
+    @Test
+    void testFind() {
+        var resultStream = testStream.map(
+                pr -> anyServiceStream.serviceCall(pr)
+            )
+            .find(processingResult -> processingResult.isResult() == false);
+
+        var resultList = testList.map(
+                pr -> anyServiceList.serviceCall(pr)
+            )
+            .find(processingResult -> processingResult.isResult() == false);
+
+        verify(anyServiceStream, times(3)).serviceCall(any()); // result of implementation Stream as lazy linked list
+        verify(anyServiceList, times(6)).serviceCall(any());
+
+        assertEquals(Some(pr3), resultStream);
+        assertEquals(Some(pr3), resultList);
+    }
+
+    @Test
+    void testContinually() {
+        AnyInterface anyService = mock(AnyInterface.class);
+        when(anyService.serviceCall())
+                .thenReturn(Either.right(pr1))
+                .thenReturn(Either.right(pr2))
+                .thenReturn(Either.right(pr3));
+
+        Stream.continually(() -> anyService.serviceCall())
+                .find(result -> result.isRight() && result.get().isResult() == false);
+
+        verify(anyService, times(3)).serviceCall();
+    }
+}
---
+++ 2020-03-21-java-with-vavr-case-study/src/test/java/com/pbroda/codesnippets/vavr/WithOrWithoutIfs.java
@@ -0,0 +1,93 @@
+package com.pbroda.codesnippets.vavr;
+
+import io.vavr.collection.HashSet;
+import io.vavr.collection.Set;
+import io.vavr.control.Either;
+import io.vavr.control.Option;
+import lombok.Value;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
+
+interface AllowedLabelsService {
+    Either<Throwable, Boolean> isAllowed(String label);
+}
+
+@Value
+class LabelFilter {
+    private final Set<String> allowedLabels = HashSet.of("AB", "CD", "EF");
+    private final AllowedLabelsService allowedLabelsService;
+
+    public boolean filter(String label) {
+        if (label == null || label.isBlank()) {
+            return false;
+        }
+        if (!allowedLabels.contains(label.toUpperCase())) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean filter2(String label) {
+        return Option.of(label)
+                .map(String::toUpperCase)
+                .filter(allowedLabels::contains)
+                .map(__ -> true)
+                .getOrElse(false);
+    }
+
+    public Either<Throwable, Boolean> filter3(String label) {
+        return Option.of(label)
+                .map(allowedLabelsService::isAllowed)
+                .getOrElse(() -> Either.right(false));
+    }
+}
+
+@ExtendWith(MockitoExtension.class)
+public class WithOrWithoutIfs {
+
+    private AllowedLabelsService allowedLabelsServiceImpl;
+    private LabelFilter labelFilter;
+
+    private LabelFilter createLabelFilterTestInstance(Either<Throwable, Boolean> allowedLabelsServiceResult) {
+        allowedLabelsServiceImpl = mock(AllowedLabelsService.class);
+        lenient().when(allowedLabelsServiceImpl.isAllowed(any())).thenReturn(allowedLabelsServiceResult);
+        return new LabelFilter(allowedLabelsServiceImpl);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "GH"})
+    void testShouldReturnFalseWhenLabelNotAllowed(String label) {
+        labelFilter = createLabelFilterTestInstance(Either.right(false));
+        assertFalse(labelFilter.filter(label));
+        assertFalse(labelFilter.filter2(label));
+        assertFalse(labelFilter.filter3(label).get());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"AB", "CD", "EF", "ab"})
+    void testShouldReturnTrueWhenLabelIsAllowed(String label) {
+        labelFilter = createLabelFilterTestInstance(Either.right(true));
+        assertTrue(labelFilter.filter(label));
+        assertTrue(labelFilter.filter2(label));
+        assertTrue(labelFilter.filter3(label).get());
+    }
+
+    @Test
+    void testShouldReturnLeftWhenLabelInfoServiceReturnsLeft() {
+        var expectedException = new Exception();
+        labelFilter = createLabelFilterTestInstance(Either.left(expectedException));
+        assertEquals(labelFilter.filter3("anyLabel").getLeft(), expectedException);
+    }
+}
